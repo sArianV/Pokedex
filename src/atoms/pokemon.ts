97545bb67281f0e595e717e0183ebcb9058c8e71
sArianV <arian94.dev@gmail.com>
2023-03-08 13:52:43 -0300

    search in pokedex

--- src/atoms/pokemon.ts
+++ src/atoms/pokemon.ts
@@ -1,18 +1,21 @@
-import { TPokemon, TPokdex } from '@controllers/types'
-import { atom } from 'jotai'
+import { TPokdex } from "@controllers/types";
+import { atom } from "jotai";
+import { isNaN } from "lodash";
 
-export const PokedexAtom = atom<TPokdex>([])
-export const PokedexInput =atom<string>('')
-export const PokedexLenght = atom<number>(8)
+export const PokedexAtom = atom<TPokdex>([]);
+export const PokedexInput = atom<string>("");
+export const PokedexLenght = atom<number>(8);
 
 export const PokemonSearchAtom = atom((get) => {
-    const pokedex = get(PokedexAtom) as TPokdex
-    const input = get(PokedexInput)
-    const lenght = get(PokedexLenght)
+  const pokedex = get(PokedexAtom) as TPokdex;
+  const input = get(PokedexInput);
+  const lenght = get(PokedexLenght);
 
-    if (!pokedex) return []
-    if ( input === "" ) return pokedex.slice(0, lenght)
-
-    
-    return null
-})
+  if (!pokedex) return [];
+  if (input === "") return pokedex.slice(0, lenght);
+  if (!isNaN(Number(input)))
+    return pokedex.filter((pokemon) => pokemon.id.toString().includes(input)).slice(0, lenght);
+  return pokedex.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(input.toLowerCase())
+  ).slice(0, lenght);
+});
--- src/formaters/formatPokedex.ts
+++ src/formaters/formatPokedex.ts
@@ -4,10 +4,10 @@ const formatPokedex = (pokedex: TPokdex) => {
   return pokedex.map((pokemon, index) => {
     return {
       id: `${index + 1}`,
-      name: pokemon.name,
-      url: pokemon.url,
+      name: pokemon.name || "",
+      url: pokemon.url || "",
     };
-  });
+  }).slice(0, 1008);
 };
 
 export default formatPokedex;
